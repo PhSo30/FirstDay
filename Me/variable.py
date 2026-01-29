b4104a24a30cfcf9da6946989f47e03255d47835
Sophea <sopheaisanidiot@gmail.com>
2026-01-29 09:36:09 +0700

    var

---
+++ Me/variable.py
@@ -0,0 +1,14 @@
+# Don't need to specify type
+number1 = 25; #int
+number2 = 2.44; #float
+text1 = "Hello!" #string
+
+print(number1, number2, text1)
+
+# if you want to specify you can use cast
+
+number1 = int(25)
+number2 = float(2.44)
+text1 = str("Hello!")
+
+print(number1, number2, text1)
